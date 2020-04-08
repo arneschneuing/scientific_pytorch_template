cada3afc359e413f7c36d07055ffc7e4958125ef
arne <arne.schneuing@rwth-aachen.de>
2020-04-08 13:04:03 +0200

    add script to copy relevant template files and initialize a new project

---
+++ copy_template.py
@@ -0,0 +1,28 @@
+from shutil import copytree, ignore_patterns
+import os
+import argparse
+
+# Argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("dest", type=str, nargs='?', default='../new_project')
+args = parser.parse_args()
+destination = args.dest
+
+# Get current directory
+template_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Make sure the right folder is copied
+assert os.path.basename(template_dir) == 'scientific_pytorch_template'
+
+# Read gitignore
+gitignore = os.path.join(template_dir, '.gitignore')
+with open(gitignore) as f:
+    ignore = f.readlines()
+ignore = [x.strip('\n').strip('/') for x in ignore]
+
+# Add further items to ignore
+ignore += ['.gitignore', '.git', 'copy_template.py']
+
+# Copy files to new location
+copytree(template_dir, destination, ignore=ignore_patterns(*ignore))
+print(f'Copied template from {template_dir} to {os.path.abspath(destination)}')
