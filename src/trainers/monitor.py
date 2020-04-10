a5f90058440bfec0d9d213bf0d087d44c99947cc
arne <arne.schneuing@rwth-aachen.de>
2020-04-10 12:56:49 +0200

    handle immediate end of training, monitor iteration counter is now incremented after end of training flag is set

--- src/trainers/monitor.py
+++ src/trainers/monitor.py
@@ -26,8 +26,8 @@ class Monitor:
         self._counter = 0
 
         # Initialization best state for model saving
-        self._best_score = None
-        self._best_iteration = None
+        self._best_score = 0
+        self._best_iteration = 0
 
         # Initialize iteration to zero
         self.it = 0
@@ -38,11 +38,11 @@ class Monitor:
 
         # Set early stopping patience if specified
         # Default: No early stopping
-        self._patience = self._cfg['Train'].get('patience',
-                                                self.num_iterations)
+        self._patience = self._cfg.get('patience', self.num_iterations)
 
         # Initialize monitor flags
-        self.flags = MonitorFlags(False, False, False)
+        end_training = (self.num_iterations - self._val_freq < 0)
+        self.flags = MonitorFlags(False, False, end_training)
 
     def register_result(self, score):
 
@@ -126,11 +126,9 @@ class Monitor:
 
     def step(self):
         """
-        Update state of the monitor. Do not increase iteration counter if
-        "end-of-training"-flag is set.
+        Update state of the monitor.
         """
-        if not self.flags.end_training:
-            self.it += 1
+        self.it += 1
 
     def _get_scheduling_params(self, cfg):
         """
@@ -147,12 +145,13 @@ class Monitor:
         """
 
         # Read from config file
-        num_epochs = cfg['Train'].get('num_epochs', None)
-        num_iterations = cfg['Train'].get('num_iterations', None)
-        checkpoint_freq = cfg['Train'].get('checkpoint_freq', None)
-        val_freq = cfg['Train'].get('val_freq', None)
-        lr_freq = cfg['Train'].get('lr_freq', None)
-        log_freq = cfg['Train'].get('log_freq', None)
+        num_epochs = cfg.get('num_epochs', None)
+        num_iterations = cfg.get('num_iterations', None)
+        checkpoint_freq = cfg.get('checkpoint_freq', None)
+        val_freq = cfg.get('val_freq', None)
+        log_freq = cfg.get('log_freq', None)
+        lr_freq = cfg['LR_Scheduler'].get('lr_freq', None) \
+            if cfg.get('LR_Scheduler', False) else None
 
         # Check for missing information
         if num_epochs is None and num_iterations is None:
@@ -167,6 +166,7 @@ class Monitor:
         # Set unit for training
         if num_iterations is None:
             # epoch-based training
+            epoch_based = True
             num_iterations = self.batches_per_epoch * num_epochs
             checkpoint_freq = num_iterations if checkpoint_freq is None \
                 else self.batches_per_epoch * checkpoint_freq
@@ -174,24 +174,19 @@ class Monitor:
                 else self.batches_per_epoch * val_freq
             lr_freq = self.batches_per_epoch if lr_freq is None \
                 else self.batches_per_epoch * lr_freq
-            epoch_based = True
         else:
             # iteration-based training
             epoch_based = False
-            checkpoint_freq = self.batches_per_epoch if checkpoint_freq \
-                                                        is None \
+            checkpoint_freq = num_iterations if checkpoint_freq is None \
                 else checkpoint_freq
-            val_freq = self.batches_per_epoch if val_freq is None else val_freq
-            lr_freq = self.batches_per_epoch if lr_freq is None else lr_freq
+            val_freq = num_iterations if val_freq is None else val_freq
+            lr_freq = 1 if lr_freq is None else lr_freq
 
         # Set logging frequency to 10 if no value provided
         if log_freq is None:
             log_freq = 10
 
         # Check validity of provided frequencies
-        assert num_iterations >= self.batches_per_epoch, 'Training duration ' \
-                                                         'has to be longer ' \
-                                                         'than one epoch!'
         assert checkpoint_freq % val_freq == 0, 'Checkpoint frequency ' \
                                                 'has to be an integer ' \
                                                 'multiple of the ' \
@@ -218,12 +213,15 @@ class Monitor:
 
     def summary_string(self):
         """
-        Log a summary string
+        Create a summary string
         """
 
         if self.epoch_based:
             # Get number of current epoch and iteration
-            epoch, epoch_it = self.i2e()
+            if self.it == 0:
+                epoch, epoch_it = 0, 0
+            else:
+                epoch, epoch_it = self.i2e(self.it - 1)
 
             # Set progress string
             prog_string = f'Epoch: {epoch} | Iteration: {epoch_it}/' \
@@ -232,22 +230,26 @@ class Monitor:
             # Set best model string
             best_epoch, best_epoch_it = self.i2e(self._best_iteration)
             if best_epoch_it != self.batches_per_epoch:
-                epoch_string = f'{best_epoch-1} epochs and {best_epoch_it} ' \
-                               f'iterations '
+                epoch_string = f'{best_epoch-1} epoch(s) and {best_epoch_it} ' \
+                               f'iteration(s) '
             else:
-                epoch_string = f'{best_epoch} epochs '
+                epoch_string = f'{best_epoch} epoch(s) '
             best_model_string = f'Best model trained for ' \
                                 + epoch_string + \
                                 f'({self._best_iteration + 1} ' \
-                                f'iterations in total) and achieved score of' \
-                                f' {self._best_score:.4f}.\n'
+                                f'iteration(s) in total) and achieved score ' \
+                                f'of {self._best_score:.4f}.\n'
         else:
             # Set progress string
-            prog_string = f'Iteration: {self.it + 1}/{self.num_iterations}'
+            prog_string = f'Iteration: {self.it}/{self.num_iterations}'
 
             # Set best model string
+            if self.it == 0:
+                best_iteration = 0
+            else:
+                best_iteration = self._best_iteration + 1
             best_model_string = f'Best model trained for ' \
-                                f'{self._best_iteration + 1} iterations ' \
+                                f'{best_iteration} iterations ' \
                                 f'achieved score of {self._best_score:.4f}.\n'
 
         if self._counter > self._patience:
